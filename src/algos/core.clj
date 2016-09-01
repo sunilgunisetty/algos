fd90a3f8e20fda3908904102cc0d3556af18817f
svg <svg@broadpeakpartners.com>
2016-09-01 13:56:34 -0400

    Implemented sum of subset problem

--- src/algos/core.clj
+++ src/algos/core.clj
@@ -1,6 +1,30 @@
 (ns algos.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 1. Sum of subset problem
+;; Given a set and a num, find a subset which sums to the number.
+;; Example #{1 2 3 4 5} 6 =>> #{4 2}
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn sum-of-subset
+  "set = set of numbers
+   sum = total sum
+   subset = subset which adds to sum"
+  [set sum subset]
+  (cond
+    (= sum 0) {:status :success :res subset}
+    (or (< sum 0) (empty? set)) {:status :fail :res #{}}
+    :else
+    (let [res1 (sum-of-subset (rest set) (- sum (first set)) (conj subset (first set)))
+          res2 (sum-of-subset (rest set) sum subset)]
+      (cond
+        (and (= (:status res1) :fail)
+             (= (:status res2) :fail)) {:status :fail :res #{}}
+        (= (:status res2) :success) {:status :success :res (:res res2)}
+        (= (:status res1) :success) {:status :success :res (:res res1)}
+        :else {:status :success
+               :res (if (> (count (:res res1)) (count (:res res2)))
+                      (:res res2)
+                      (:res res1))}))))
--- test/algos/core_test.clj
+++ test/algos/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [algos.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest sum-of-subset-test
+  (testing "Sum of Subset"
+    (is (= 6 (apply + (:res (sum-of-subset #{1 2 3 4 5} 6 #{})))))
+    (is (= 17 (apply + (:res (sum-of-subset #{1 2 3 4 5 9 8 7} 17 #{})))))
+    (is (= 27 (apply + (:res (sum-of-subset #{1 2 3 4 5 9 8 7} 27 #{})))))
+    (is (= 37 (apply + (:res (sum-of-subset #{1 2 3 4 5 9 8 7} 37 #{})))))
+    (is (= :fail (:status (sum-of-subset #{1 2 3 4 5 9 8 7} 47 #{}))))))
